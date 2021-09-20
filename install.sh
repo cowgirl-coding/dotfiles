9fd2581e8e2c32f293f6d583b6e49728f2f6e05e
Kevin Waddle <bonzobavaria@gmail.com>
2021-09-19 21:37:56 -0500

    Add install script

---
+++ install.sh
@@ -0,0 +1,11 @@
+# Add directories to .config
+ln -s ./rofi $HOME/.config/rofi
+ln -s ./sxhkd $HOME/.config/sxhkd
+ln -s ./bspwm $HOME/.config/bspwm
+
+# Add top-level configs
+ln -s ./.vimrc $HOME/.vimrc
+ln -s ./.tmux.conf $HOME/.tmux.conf
+ln -s ./.ghci $HOME/.ghci
+ln -s ./.gitconfig $HOME/.gitconfig
+ln -s ./.aliases $HOME/.aliases
